038432309a2343fdc217baab42c00fb1da60d26c
felipedmesquita <16197684+felipedmesquita@users.noreply.github.com>
2023-03-13 15:54:34 -0300

    create schema if not exists e materialize

--- lib/dbt.rb
+++ lib/dbt.rb
@@ -6,6 +6,7 @@ require 'dagwood'
 
 module Dbt
   def self.run
+    ActiveRecord::Base.connection.execute "CREATE SCHEMA IF NOT EXISTS felipe_dbt"
     file_paths = Dir.glob("app/sql/**/*.sql")
     models = file_paths.map {|fp| Model.new fp }
     dependencies = models.map {|m| {m.name => m.refs}}.reduce({}, :merge!)
--- lib/dbt/model.rb
+++ lib/dbt/model.rb
@@ -1,6 +1,6 @@
 class Dbt::Model
 
-  attr_reader :name, :code, :sources, :refs, :built, :filepath
+  attr_reader :name, :code, :materialize_as, :sources, :refs, :built, :filepath
   def initialize filepath
     @filepath = filepath
     @name = File.basename(filepath, '.sql')
@@ -8,6 +8,7 @@ class Dbt::Model
     @sources = []
     @refs = []
     @built = false
+    @materialize_as = ""
 
     def source(table)
       @sources << table
@@ -19,14 +20,21 @@ class Dbt::Model
       "felipe_dbt.#{model}"
     end
 
+    def materialize
+      @materialize_as = "MATERIALIZED"
+      ""
+    end
+
     @code = ERB.new(@original_code).result(binding)
   end
 
   def build
     puts "BUILDING #{@name}"
     ActiveRecord::Base.connection.execute <<~SQL
+      DROP TABLE IF EXISTS felipe_dbt.#{@name} CASCADE;
+      DROP MATERIALIZED VIEW IF EXISTS felipe_dbt.#{@name} CASCADE;
       DROP VIEW IF EXISTS felipe_dbt.#{@name} CASCADE;
-      CREATE VIEW felipe_dbt.#{@name} AS (
+      CREATE #{materialize_as} VIEW felipe_dbt.#{@name} AS (
         #{@code}
       );
     SQL
