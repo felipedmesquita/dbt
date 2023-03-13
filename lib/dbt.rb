a26fb7e9cfa09e3e49e8cff1d45a3ecc704a60d3
felipedmesquita <16197684+felipedmesquita@users.noreply.github.com>
2023-03-13 12:50:04 -0300

    model error

--- lib/dbt.rb
+++ lib/dbt.rb
@@ -2,7 +2,7 @@ module Dbt
 
   def self.run
     file_paths = Dir.glob("app/sql/**/*.sql")
-    models = file_paths.map {|fp| DbtModel.new fp }
+    models = file_paths.map {|fp| Model.new fp }
     dependencies = models.map {|m| {m.name => m.refs}}.reduce({}, :merge!)
     graph = Dagwood::DependencyGraph.new dependencies
     graph.order.each do |model_name|
