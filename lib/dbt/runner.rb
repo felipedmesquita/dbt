2f7944e730d42e8c15b5473fd38c1fd932694e1c
felipedmesquita <16197684+felipedmesquita@users.noreply.github.com>
2023-05-09 17:25:30 -0300

    Revert "Merge pull request #1 from felipedmesquita/always-run-in-temp-schema"
    This reverts commit 948a65cf5d6acc4ea2397b51be159dfa32e3bff9, reversing
    changes made to 966189479ab113518ffdeda4e8fe637ae047b994.

--- lib/dbt/runner.rb
+++ lib/dbt/runner.rb
@@ -2,10 +2,9 @@ module Dbt
   class Runner
     class << self
       def run(schema = SCHEMA)
-        temp_schema = "#{schema}_temp"
-        ActiveRecord::Base.connection.execute "CREATE SCHEMA IF NOT EXISTS #{temp_schema}"
+        ActiveRecord::Base.connection.execute "CREATE SCHEMA IF NOT EXISTS #{schema}"
         file_paths = Dir.glob("app/sql/**/*.sql")
-        models = file_paths.map { |fp| Model.new(fp, temp_schema) }
+        models = file_paths.map { |fp| Model.new(fp, schema) }
         dependencies =
           models.map { |m| { m.name => m.refs } }.reduce({}, :merge!)
         check_if_all_refs_have_sql_files dependencies
@@ -15,7 +14,6 @@ module Dbt
         graph.order.each do |model_name|
           models.find { |m| m.name == model_name }.build
         end
-        drop_old_schema_and_rename_temp temp_schema, schema
       end
 
       def check_if_all_refs_have_sql_files(dependencies)
@@ -25,11 +23,6 @@ module Dbt
             raise "Missing .sql model files for ref #{sem_arquivo} in model #{key}"
           end
         end
-      end
-
-      def drop_old_schema_and_rename_temp temp, schema
-        ActiveRecord::Base.connection.execute "DROP SCHEMA IF EXISTS #{schema} CASCADE"
-        ActiveRecord::Base.connection.execute "ALTER SCHEMA #{temp} RENAME TO #{schema}"
       end
     end
   end
